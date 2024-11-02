b7791257aee6d41f1548feaec72b5ac547979371
Ryder Klein <ryder679@live.com>
2024-11-02 14:19:49 -0400

    test

--- MissileGame/MissileGame/MissileGameApp.swift
+++ MissileGame/MissileGame/MissileGameApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct MissileGameApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
--- MissileGame/MissileGame/Shared/AppDelegate.swift
+++ MissileGame/MissileGame/Shared/AppDelegate.swift
@@ -8,5 +8,28 @@ import Foundation
 import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    
+    var notificationsRegistered = false
+    var deviceToken: String? = nil
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        application.registerForRemoteNotifications()
+        return true
+    }
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        notificationsRegistered = true
+        self.deviceToken = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        print("Successfully registered notifications")
+        print(self.deviceToken!)
+//        self.sendDeviceTokenToServer(data: deviceToken)
+    }
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: any Error) {
+        notificationsRegistered = false
+        print("Failed to register notifications")
+        print(error.localizedDescription)
+        Task {
+            try? await Task.sleep(for: .seconds(5))
+            application.registerForRemoteNotifications()
+        }
+//        Handle failed
+    }
 }
+
