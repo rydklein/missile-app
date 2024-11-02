b7f9dbbfae3a51f7124e1ac4f61f969cad058e77
Ryder Klein <ryder679@live.com>
2024-11-02 13:27:19 -0400

    Create shared folder

---
+++ MissileGame/MissileGame/Shared/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  Notifications.swift
+//  MissileGame
+//
+//  Created by Ryder Klein on 11/2/24.
+//
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+}
