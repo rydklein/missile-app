ed59b7356df649484aca1f85546a64f16dd518b5
William Chesser <bchesser@unc.edu>
2024-11-02 19:20:02 -0400

    Map fixes
    yuh

--- MissileGame/MissileGame/Gameplay/MapView.swift
+++ MissileGame/MissileGame/Gameplay/MapView.swift
@@ -8,9 +8,8 @@
 import MapKit
 import SwiftUI
 
-
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(
+    private let region: MapCameraPosition = .region(MKCoordinateRegion(
         center: CLLocationCoordinate2D(
             latitude: LocationManager.shared.userLocation?.coordinate.latitude ?? 00,
             longitude: LocationManager.shared.userLocation?.coordinate.longitude ?? 00
@@ -20,35 +19,39 @@ struct MapView: View {
             longitudeDelta: 0.2
         )
     )
+    )
     @State private var pinLocation = LocationManager.shared.userLocation?.coordinate
+    @Namespace var mapScope
     var body: some View {
-        VStack {
+        VStack(spacing: .zero) {
             ZStack {
-                MapReader{ reader in
-                    Map(coordinateRegion: $region, interactionModes: [.all], showsUserLocation: true)
-                        .edgesIgnoringSafeArea(.all)
-                        .onTapGesture(perform: { screenCoord in
-                            pinLocation = reader.convert(screenCoord, from: .local)
-                            print(pinLocation)
-                        })
-                    // .mapStyle(.mutedStandard)
-                }
+                MapReader { reader in
+                    Map(initialPosition: .userLocation(fallback: region), interactionModes: [.all], scope: mapScope) {
+                        UserAnnotation()
+                    }
+                    .onTapGesture(perform: { screenCoord in
+                        pinLocation = reader.convert(screenCoord, from: xxw.local)
+                        print(pinLocation)
+                    })
+                    .mapControls {
+                        MapUserLocationButton(scope: mapScope)
+                    }
                     LinearGradient(gradient: Gradient(colors: [.purple, .white, .blue]),
                                    startPoint: .top,
                                    endPoint: .bottom)
-                    .opacity(0.3)
-                    .edgesIgnoringSafeArea(.all)
-                    .allowsHitTesting(false)
-                
+                        .opacity(0.3)
+                        .edgesIgnoringSafeArea(.all)
+                        .allowsHitTesting(false)
+                }
+                ToolbarView()
             }
-            
-            ToolbarView()
+            .mapScope(mapScope)
         }
-            .ignoresSafeArea()
     }
 }
 
 #Preview {
-    MapView()
-        .ignoresSafeArea()
+    NavigationStack {
+        MapView()
+    }
 }
