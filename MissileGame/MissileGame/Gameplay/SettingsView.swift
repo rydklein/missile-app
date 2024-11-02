9641d2457db1b0b90d72b19261cf7dd7353de634
asimraja <asimraja@apps-air.wireless-1x.unc.edu>
2024-11-02 17:21:30 -0400

    PhaseTimeView and SettingsView init. Also added PrepToolbar to Map

---
+++ MissileGame/MissileGame/Gameplay/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  MissileGame
+//
+//  Created by asimraja on 11/2/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
