ebd6eba31a83f1d567d80ce1b0f547e14de6fe21
William Chesser <bchesser@unc.edu>
2024-11-02 12:39:20 -0400

    poop fart

--- MissileGame/MissileGame/MissileGameApp.swift
+++ MissileGame/MissileGame/MissileGameApp.swift
@@ -2,31 +2,16 @@
 //  MissileGameApp.swift
 //  MissileGame
 //
-//  Created by Ryder Klein on 11/2/24.
+//  Created by Ben Chesser on 11/2/24.
 //
 
 import SwiftUI
-import SwiftData
 
 @main
 struct MissileGameApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
