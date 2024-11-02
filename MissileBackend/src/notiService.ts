7d060a046a934343adf67c3fd308e1793a4dc747
Ryder Klein <ryder679@live.com>
2024-11-02 16:21:22 -0400

    Reorganize files

--- MissileBackend/src/notiService.ts
+++ MissileBackend/src/notiService.ts
@@ -1,41 +1,41 @@
-import apn from '@parse/node-apn'
-import cron from 'node-cron';
-import dotenv from 'dotenv'
-dotenv.config()
+import apn from "@parse/node-apn";
+import cron from "node-cron";
+import dotenv from "dotenv";
+dotenv.config();
 
 const options: apn.ProviderOptions = {
-    token: {
-      key: "./path", 
-      keyId: "YOUR_KEY_ID", 
-      teamId: "YOUR_TEAM_ID", 
-    },
-    production: false,
-  }
+  token: {
+    key: "./path",
+    keyId: "YOUR_KEY_ID",
+    teamId: "YOUR_TEAM_ID",
+  },
+  production: false,
+};
 
-
-const apnProvider = new apn.Provider(options)
+const apnProvider = new apn.Provider(options);
 
 export const startDailyMissileNotification = () => {
-    cron.schedule('0 9 * * *', async () => {
-      await sendMissileNotification()
-    }, {
-      timezone: 'America/New_York',
-    })
-  }
+  cron.schedule(
+    "0 9 * * *",
+    async () => {
+      await sendMissileNotification();
+    },
+    {
+      timezone: "America/New_York",
+    }
+  );
+};
 
 async function sendMissileNotification() {
-    const notification = new apn.Notification()
-    notification.topic = process.env.APP_BUNDLE!
-    notification.aps = { "content-available": 1 }
+  const notification = new apn.Notification();
+  notification.topic = process.env.APP_BUNDLE!;
+  notification.aps = { "content-available": 1 };
 
-    notification.payload = {
-        updateType: "missileLocationUpdate",
-      } 
+  notification.payload = {
+    updateType: "missileLocationUpdate",
+  };
 }
 
-
-
-
-process.on('exit', () => {
-    apnProvider.shutdown()
-  })
+process.on("exit", () => {
+  apnProvider.shutdown();
+});
--- MissileBackend/src/routes/userRoutes.ts
+++ MissileBackend/src/routes/userRoutes.ts
@@ -1,55 +1,54 @@
-import { Router, Request, Response } from 'express'
-import { PrismaClient } from '@prisma/client'
+import { Router, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
-const router = Router()
-const prisma = new PrismaClient()
+const router = Router();
+const prisma = new PrismaClient();
 
-router.get('/', async (req, res) => {
-    try {
-      const users = await prisma.user.findMany()
-      res.status(200).json(users)
-    } catch (error) {
-      res.status(500).json({ error: 'Error fetching posts' })
+router.get("/", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching posts" });
+  }
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  const { deviceIdentifier, deviceToken } = req.body;
+
+  try {
+    const user = await prisma.user.create({
+      data: { id: deviceIdentifier, device_token: deviceToken, name: null },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Error creating user" });
+  }
+});
+
+router.put("/", async (req: Request, res: Response) => {
+  const { deviceIdentifier, deviceToken, userName } = req.body;
+
+  try {
+    const data: { name?: string; deviceToken?: string } = {};
+
+    if (userName) {
+      data.name = userName;
     }
-  })
+    if (deviceToken) {
+      data.deviceToken = deviceToken;
+    }
 
-router.post('/', async (req: Request, res: Response) => {
-    const { deviceIdentifier, deviceToken } = req.body;
-
-    try {
-        const user = await prisma.user.create({
-          data: { id: deviceIdentifier, device_token: deviceToken, name: null },
-        })
-        res.status(201).json(user)
-      } catch (error) {
-        res.status(500).json({ error: 'Error creating user' })
-      }
-})
-
-router.put('/', async (req: Request, res: Response) => {
-    const { deviceIdentifier, deviceToken, userName } = req.body;
-
-    try {
-        const data: { name?: string; deviceToken?: string } = {}
-
-        if (userName) {
-            data.name = userName
-        }
-        if (deviceToken) {
-            data.deviceToken = deviceToken
-        }
-
-        const user = await prisma.user.update({
-            where: {
-                id: deviceIdentifier,
-              },
-              data
-        })
-        res.status(200).json(user)
-      } catch (error) {
-        res.status(500).json({ error: 'Error updating user' })
-      }
-})
-
+    const user = await prisma.user.update({
+      where: {
+        id: deviceIdentifier,
+      },
+      data,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Error updating user" });
+  }
+});
 
 export default router;
