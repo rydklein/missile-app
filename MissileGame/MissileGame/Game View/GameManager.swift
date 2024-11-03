75f078aade94ffe030412ebce840bf95d08efa44
Ryder Klein <ryder679@live.com>
2024-11-02 22:29:24 -0400

    GameManager

--- MissileGame/MissileGame/Game View/GameManager.swift
+++ MissileGame/MissileGame/Game View/GameManager.swift
@@ -10,9 +10,12 @@ import Foundation
 import MapKit
 
 @Observable class GameManager {
+    public var shared = GameManager()
+    
     public var endTime = Date(timeIntervalSinceNow: 60 * 60 * 8)
     public var myMissileLocation: CLLocationCoordinate2D?
     public var myShieldLocation: CLLocationCoordinate2D?
+    public var healthPoints: Int = 3
     public var incomingMissile: IncomingMissile?
     public var gameState: GameState = .planning
     
@@ -21,11 +24,11 @@ import MapKit
     }
     
     public func placeMissile(lat: Double, long: Double) {
-        
+        myMissileLocation = CLLocationCoordinate2D(latitude: lat, longitude: long)
     }
     
     public func placeShield(lat: Double, long: Double) {
-        
+        myShieldLocation = CLLocationCoordinate2D(latitude: lat, longitude: long)
     }
 }
 
